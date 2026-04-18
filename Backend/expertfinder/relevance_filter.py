d0791f0a6568e70a909a6fe479671a6a449bffa0
vengadesa <vengiboopathi@gmail.com>
2026-04-18 17:31:07 +0530

    created functions to filter post which contains relevant information based on average cosine similarity with expertise signals generated by an llm

---
+++ Backend/expertfinder/relevance_filter.py
@@ -0,0 +1,33 @@
+from .subreddit_validator import cosine_similarity
+from ..rag.embeddings import embed_documents
+import asyncio
+
+def group_by_author(content: list[dict]) -> dict[str, list[dict]]:
+    result = {}
+    for post in content:
+        author = post["author"]
+        if author not in result:
+            result[author] = []
+        result[author].append(post)
+    return result
+
+async def filter_relevant_content(content:list[dict],expertise_signals:list[str],threshold:float =0.3)-> dict[str, list[dict]]:
+    if not content or not expertise_signals:
+        return {}
+
+    filtered_post =[]
+    content_texts = [post["text"] for post in content]
+    expertise_signals_embeddings,content_embeddings = await asyncio.gather(embed_documents(expertise_signals),embed_documents(content_texts))
+    
+    for j, post_embedding in enumerate(content_embeddings):
+        scores = [
+            cosine_similarity(post_embedding, sig_emb) for sig_emb in expertise_signals_embeddings
+        ]
+        avg_score = sum(scores) / len(scores)
+        if avg_score >= threshold:
+            filtered_post.append(content[j])
+    
+    return group_by_author(filtered_post)
+    
+
+
