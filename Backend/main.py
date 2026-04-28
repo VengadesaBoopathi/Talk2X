6b392e3975c72186862001fde5880088ab0216ad
VengadesaBoopathi <vengiboopathi@gmail.com>
2026-04-28 07:55:30 +0530

    feat(ui, sse, filtering, model): - Implemented SSE to stream intermediate output to frontend - Added filter to remove unnecessary text from LLM responses - Improved expert retrieval efficiency with stricter threshold and blocking mods/bots - Switched model to gemini-2.5-flash-lite for cost optimization (pay-per-use) - seccret removed

--- Backend/main.py
+++ Backend/main.py
@@ -2,10 +2,7 @@ from fastapi import FastAPI
 from .api.routes import router
 from .api.expert_router import router as expert_router
 from fastapi.middleware.cors import CORSMiddleware
-import os
-os.environ["CURL_CA_BUNDLE"] = ""
-os.environ["PYTHONHTTPSVERIFY"] = "0"
-os.environ["HF_TOKEN"] = "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"
+
 
 app = FastAPI(title ="Talk2X",version="1.0.0")
 
